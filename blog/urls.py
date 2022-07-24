c4918fac016e4ec4c40b4be2a148f24709f84f0d
Josh Nagel <nageljp27@gmail.com>
2022-07-24 11:48:52 -0600

    added the skeleton for urls and views for the blog app. Wired the urls path in the project urls.

--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,11 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    # load starting page with lists of latest blog posts and some welcome text
+    path('', views.index, name='index-page'),  # / path
+    # load page which lists all blog posts
+    path('posts', views.posts, name='posts-page'),  # /posts
+    # load individual blog post page which shows full blog post
+    path('posts/<slug:slug>', views.post_detail, name='post-detail-page')  # /posts/my-blog-post
+]
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render
+
+
+def index(request):
+    return render(request, '')
+
+
+def posts(request):
+    pass
+
+
+def post_detail(request):
+    pass
