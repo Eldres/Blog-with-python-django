02c56f793020acf15d0a3458721ac995b6bbff4c
Josh Nagel <nageljp27@gmail.com>
2022-07-25 09:31:05 -0600

    created the Post, Author(foreign_key) and Tag models. Registered them in the admin file and then ran the migrations commands

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,35 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 
-class Blog(models.Model):
-  title = models.CharField(max_length=100)
-  author = models.CharField(max_length=50)
-  date = models.DateField(auto_now_add=True)
-  image = models.FileField()
-  slug = models.SlugField(allow_unicode=True)
-  content = models.TextField(default='')
-  excerpt = models.CharField(max_length=150)
 
-  def __str__(self):
-    return f"{self.title} - {self.author} ({self.date}): {self.excerpt}"
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.email}"
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.caption
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    tag = models.ManyToManyField(Tag)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts", default='')
+    date = models.DateField(auto_now=True)
+    # image = models.FileField()
+    image_name = models.CharField(max_length=100, default='')
+    slug = models.SlugField(unique=True, allow_unicode=True)
+    content = models.TextField(default='')
+    excerpt = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.title} - {self.author} ({self.date}): {self.excerpt}"
