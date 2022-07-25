b19aee9f86e60ad5604dcbef702dcbc8424e6665
Josh Nagel <nageljp27@gmail.com>
2022-07-25 13:27:50 -0600

    converted views to Class-based views

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     # load starting page with lists of latest blog posts and some welcome text
-    path('', views.index, name='index-page'),  # / path
+    # path('', views.index, name='index-page'),  # / path
+    path("", views.IndexPageView.as_view(), name="index-page"),
     # load page which lists all blog posts
-    path('all-posts', views.posts, name='posts-page'),  # /posts
+    # path('all-posts', views.posts, name='posts-page'),  # /posts
+    path("all-posts", views.AllPostsView.as_view(), name="posts-page"),
     # load individual blog post page which shows full blog post
-    path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),  # /posts/my-blog-post
+    # path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),  # /posts/my-blog-post
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,54 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
-def index(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]  # orders in DESC order using the `-`
-    return render(request, 'blog/index.html', {
-        "posts": latest_posts
-    })
+class IndexPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date", ]
+    # setting this field to match the previous func `posts` being sent to the context so our template views don't need to be updated
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+# def index(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]  # orders in DESC order using the `-`
+#     return render(request, 'blog/index.html', {
+#         "posts": latest_posts
+#     })
+
+class AllPostsView(ListView):
+    template_name: str = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date", ]
+    context_object_name = "all_posts"
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {
+#         "all_posts": all_posts
+#     })
 
 
-def post_detail(request, slug):
-    found_post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post-detail.html', {
-        "post": found_post,
-        "post_tags": found_post.tag.all()
-    })
+class SinglePostView(DetailView):
+    template_name: str = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tag.all()
+        return context
+
+
+# def post_detail(request, slug):
+#     found_post = get_object_or_404(Post, slug=slug)
+#     return render(request, 'blog/post-detail.html', {
+#         "post": found_post,
+#         "post_tags": found_post.tag.all()
+#     })
