9ff76666d5dd6cb082aba39a80a8eb45c15f37e0
Josh Nagel <nageljp27@gmail.com>
2022-07-25 07:31:40 -0600

    created the Blog table model and ran some migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+  title = models.CharField(max_length=100)
+  author = models.CharField(max_length=50)
+  date = models.DateField(auto_now_add=True)
+  image = models.FileField()
+  slug = models.SlugField(allow_unicode=True)
+  content = models.TextField(default='')
+  excerpt = models.CharField(max_length=150)
+
+  def __str__(self):
+    return f"{self.title} - {self.author} ({self.date}): {self.excerpt}"
